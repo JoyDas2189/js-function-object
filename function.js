db40a48996c7fe607b0f1367cdf35418e76d561f
Joy <joydas21891920@gmail.com>
2024-02-01 16:32:42 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,6 @@
+function startFan() {
+    console.log("Stand up");
+    console.log("Walk toward the swaitch");
+    console.log("Turn the swaitch on");
+}
+startFan();
---
+++ parameter.js
@@ -0,0 +1,21 @@
+/* function bringShingara(money) {
+    console.log("Ei nen Shingara.");
+    console.log(`Apner hoise ${money} Taka.`);
+}
+bringShingara(100);
+
+ */
+/* 
+function add (a, b) {
+    total = a + b;
+    console.log(`Total is ${total}`);
+}
+add(20, 40);
+ */
+
+
+var average = (a, b, c, d, e) => {
+    average = ((a + b + c + d + e)/5);
+    console.log(`Average of the folowing numbers are : ${average}`);
+}
+average(1, 2, 3, 4, 5);
